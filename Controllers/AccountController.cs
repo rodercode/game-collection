7379a26953b60a477ba15607f187716f75db78d3
rodersnow <rodersnow@gmail.com>
2024-01-28 11:40:58 +0100

    add HttpGet attritbute to Registration action method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -16,6 +16,7 @@ namespace game_collection.Controllers
             this.userManager = userManager;
         }
 
+        [HttpGet]
         public IActionResult Registration()
         {
             return View();
