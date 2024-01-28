d43fd84430f03020d73f3db3f2f909bd6113bd8b
rodersnow <rodersnow@gmail.com>
2024-01-28 11:43:56 +0100

    import identity and models to accountController

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using game_collection.Models;
 
 namespace game_collection.Controllers
 {
